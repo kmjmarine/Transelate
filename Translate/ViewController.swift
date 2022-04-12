1514c5ccbb5f410e0f4aec8226674b5c4b9e57a0
kmjmarine <kmjmarine@naver.com>
2022-04-12 18:00:19 +0900

    first deploy

---
+++ Translate/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Translate
+//
+//  Created by kmjmarine on 2022/04/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        view.backgroundColor = .red
+    }
+}
+
