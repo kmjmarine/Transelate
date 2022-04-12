081492ed2f02c4de1583e393f0a0167ef7614d93
kmjmarine <kmjmarine@naver.com>
2022-04-12 18:34:35 +0900

    add transelateView

---
+++ Translate/Scenes/TabBarController.swift
@@ -0,0 +1,34 @@
+//
+//  TabBarController.swift
+//  Translate
+//
+//  Created by kmjmarine on 2022/04/12.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        //번역화면
+        let translateViewContoller = UIViewController()
+        translateViewContoller.tabBarItem = UITabBarItem(
+            title: "번역",
+            image: UIImage(systemName: "mic"),
+            selectedImage: UIImage(systemName: "mic.fill")
+        )
+        
+        //북마크화면
+        let bookmarkViewController = UIViewController()
+        bookmarkViewController.tabBarItem = UITabBarItem(
+            title: "즐겨찾기",
+            image: UIImage(systemName: "star"),
+            selectedImage: UIImage(systemName: "star.fill")
+        )
+        
+        viewControllers = [translateViewContoller, bookmarkViewController]
+    }
+}
+
---
+++ Translate/Scenes/TranslateViewController.swift
@@ -0,0 +1,8 @@
+//
+//  TranslateViewController.swift
+//  Translate
+//
+//  Created by kmjmarine on 2022/04/12.
+//
+
+import Foundation
